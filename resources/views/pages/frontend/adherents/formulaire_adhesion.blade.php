5c8ef46c0337ff25c3ae45787664dfb45f341fba
Nel226 <cornelie.ouango@gmail.com>
2024-10-30 15:57:44 +0000

    Frontend

--- resources/views/pages/frontend/adherents/formulaire_adhesion.blade.php
+++ resources/views/pages/frontend/adherents/formulaire_adhesion.blade.php
@@ -23,24 +23,27 @@
 		</div>
 		<!-- End Breadcrumbs -->
 				
-		<section class=" section " style="background: rgb(219,218,246);
-		background: linear-gradient(110deg, rgba(219,218,246,1) 21%, rgba(252,252,255,1) 84%);">
-			<div class="container">
-				<div class="">
-					<div class="row "> 
+		<section class="section bg-gray-50">
+			<div class="container mx-auto px-4 lg:px-8">
+				<div class="bg-white shadow-lg rounded-lg overflow-hidden transform transition duration-500 hover:shadow-2xl hover:scale-105">
+					<!-- Image de fond atténuée avec Overlay translucide -->
+					<div class="relative h-64 bg-cover bg-center rounded-t-lg" style="background-image: url('{{ asset('images/caroussel/caroussel7(1).png') }}');">
+						<div class="absolute inset-0 bg-gradient-to-r from-white to-transparent opacity-70"></div>
+					</div>
+	
+					<!-- Contenu Principal -->
+					<div class="px-8 py-12 text-gray-700">
+						<h3 class="text-2xl font-semibold text-center text-gray-800 mb-6">Formulaire d'adhésion</h3>
 						
-						<div class="col-lg-12">
-                            <!-- Form -->
-                            <livewire:wizard-membership />
-                            <!--/ End Form -->
-							
+						<!-- Formulaire d'Adhésion -->
+						<div class="relative z-10">
+							<livewire:wizard-membership />
 						</div>
 					</div>
 				</div>
-				
 			</div>
 		</section>
-		
+	
         <x-footer/>
  
 </x-guest-layout>

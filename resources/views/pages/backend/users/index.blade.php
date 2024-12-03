802dc2a6a2bbe82f5326d6927dba5330cec804bc
ODC9 <123161300+ODC9@users.noreply.github.com>
2024-12-03 13:55:04 +0000

    composant user/index x-breadcrumb

--- resources/views/pages/backend/users/index.blade.php
+++ resources/views/pages/backend/users/index.blade.php
@@ -37,91 +37,92 @@
                                 </x-primary-button>
                             </a>
                         </div>
-                        <div class="relative overflow-x-auto mt-4 shadow-md rounded-lg">
-                            <table class="table-auto w-full rounded-md">
-                                <thead class="rounded-md">
-                                    <tr class="bg-gray-300">
-                                        <th class="px-4 py-2">ID</th>
-                                        <th class="px-4 py-2">Nom</th>
-                                        <th class="px-4 py-2">Email</th>
-                                        <th class="px-4 py-2">Mot de passe</th>
-                                        <th class="px-4 py-2">Rôles</th>
-                                        <th class="px-4 py-2">Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($users as $user)
-                                    <tr>
-                                        <td class="border px-4 py-2">{{ $user->id }}</td>
-                                        <td class="border px-4 py-2">{{ $user->name }}</td>
-                                        <td class="border px-4 py-2">{{ $user->email }}</td>
-                                        <td class="border px-4 py-2">
-                                            <span class="password-display">*****</span>
-                                            <button class="toggle-password ml-2 text-blue-600">
-                                                <i class="fa fa-eye"></i>
-                                            </button>
-                                            {{--  <span class="actual-password hidden">{{ $user->password_plain_text ?? 'password123' }}</span> <!-- Remplacez par le mot de passe en clair -->  --}}
-                                        </td>
-                                        <td class="border px-4 py-2">
-                                            @foreach($user->roles as $role)
-                                                {{ $role->name }}@if(!$loop->last), @endif
-                                            @endforeach
-                                        </td>
-                                        <td class="border px-4 py-2 flex gap-3 items-center text-right">
-                                            <x-primary-button class="mt-2 bg-blue-700 flex items-center space-x-2">
-                                                <a href="{{ route('users.edit', $user->id) }}" class="flex items-center text-white">
-                                                    <i class="fa fa-pencil"></i>
-                                                    <span class="ml-2">Modifier</span>
-                                                </a>
-                                            </x-primary-button>
-                                            
-                                            <!-- Bouton pour ouvrir le modal -->
-                                            <x-primary-button data-modal-id="confirmation-modal-{{ $user->id }}" class="mt-2 bg-red-700">
-                                                <i class="fa fa-trash"></i>
-                                                Supprimer
-                                            </x-primary-button>
-                                            
-                                            <!-- Modal de confirmation -->
-                                            <div id="confirmation-modal-{{ $user->id }}" class="fixed inset-0 flex items-center hidden justify-center bg-black bg-opacity-60">
-                                                <div class="bg-white text-sm rounded-md shadow-md p-6 relative w-[90%] sm:w-[460px]">
-                                                    <div class="p-3 text-center">
-                                                        <i class="fa fa-regular fa-times-circle text-red-500 mx-auto" style="font-size:48px;"></i>
-                                                        <div class="mt-5 text-2xl">Etes-vous sûr?</div>
-                                                        <div class="mt-2 text-slate-500">
-                                                            Voulez-vous vraiment supprimer l&apos;utilisateur 
-                                                            <strong>
-                                                                "{{ $user->name }}" ? <br />
-                                                            </strong>
-                                                            Cette action est irréversible.
+                        <div class="relative overflow-x-auto shadow-md rounded-lg">
+
+                            <div class="md:p-6 p-2 mx-auto bg-white rounded-lg shadow-lg ">
+            
+                                <div class="flex flex-wrap items-center justify-between gap-2 py-1 text-sm">               
+                                    <x-entries-data-table id="show-entries" />
+                                    <x-search-data-table id="table-search" />
+                    
+                                </div>
+                                
+                                <x-data-table id="table_demandes" :headers="['N°', 'ID', 'Nom', 'Email', 'Mot de passe', 'Rôles', 'Actions']">
+                                    @forelse ($users as $index => $user)
+                                        <tr class="cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700">
+                                            <td>{{ $loop->iteration }}</td> 
+                                            <td>{{ $user->id  }}</td>
+                                            <td>{{ $user->name }}</td>
+                                            <td>{{ $user->email }}</td>
+                                            <td>
+                                                <span class="password-display">*****</span>
+                                                <button class="toggle-password ml-2 text-blue-600">
+                                                    <i class="fa fa-eye"></i>
+                                                </button>
+                                            </td>
+                                            <td>
+                                                @foreach($user->roles as $role)
+                                                    {{ $role->name }}@if(!$loop->last), @endif
+                                                @endforeach
+                                            </td>
+                                            <td class="flex gap-3 items-center text-right">
+                                                <x-primary-button class="mt-2 bg-blue-700 flex items-center space-x-2">
+                                                    <a href="{{ route('users.edit', $user->id) }}" class="flex items-center text-white">
+                                                        <i class="fa fa-pencil"></i>
+                                                        <span class="ml-2">Modifier</span>
+                                                    </a>
+                                                </x-primary-button>
+                                                
+                                                <!-- Bouton pour ouvrir le modal -->
+                                                <x-primary-button data-modal-id="confirmation-modal-{{ $user->id }}" class="mt-2 bg-red-700">
+                                                    <i class="fa fa-trash"></i>
+                                                    Supprimer
+                                                </x-primary-button>
+                                                
+                                                <!-- Modal de confirmation -->
+                                                <div id="confirmation-modal-{{ $user->id }}" class="fixed inset-0 flex items-center hidden justify-center bg-black bg-opacity-60">
+                                                    <div class="bg-white text-sm rounded-md shadow-md p-6 relative w-[90%] sm:w-[460px]">
+                                                        <div class="p-3 text-center">
+                                                            <i class="fa fa-regular fa-times-circle text-red-500 mx-auto" style="font-size:48px;"></i>
+                                                            <div class="mt-5 text-2xl">Etes-vous sûr?</div>
+                                                            <div class="mt-2 text-slate-500">
+                                                                Voulez-vous vraiment supprimer l&apos;utilisateur 
+                                                                <strong>
+                                                                    "{{ $user->name }}" ? <br />
+                                                                </strong>
+                                                                Cette action est irréversible.
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                    <div class="flex items-center mx-auto justify-center px-5 pb-8 text-center">
-                                                        <div>
-                                                            <button class="close-modal transition duration-200 border shadow-sm inline-flex items-center justify-center py-2 px-3 rounded-md font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 dark:focus:ring-slate-700 dark:focus:ring-opacity-50 border-secondary text-slate-500 dark:border-darkmode-100/40 dark:text-slate-300 mr-1 w-24">
-                                                                Annuler
-                                                            </button>
-                                                        </div>
-                                                        <div>
-                                                            <!-- Formulaire de confirmation de suppression -->
-                                                            <form action="{{ route('users.destroy', $user->id) }}" method="POST" class="inline-block">
-                                                                @csrf
-                                                                @method('DELETE')
-                                                                <button type="submit" class="bg-red-600 text-white p-2 rounded">
-                                                                    Supprimer
+                                                        <div class="flex items-center mx-auto justify-center px-5 pb-8 text-center">
+                                                            <div>
+                                                                <button class="close-modal transition duration-200 border shadow-sm inline-flex items-center justify-center py-2 px-3 rounded-md font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 dark:focus:ring-slate-700 dark:focus:ring-opacity-50 border-secondary text-slate-500 dark:border-darkmode-100/40 dark:text-slate-300 mr-1 w-24">
+                                                                    Annuler
                                                                 </button>
-                                                            </form>
+                                                            </div>
+                                                            <div>
+                                                                <!-- Formulaire de confirmation de suppression -->
+                                                                <form action="{{ route('users.destroy', $user->id) }}" method="POST" class="inline-block">
+                                                                    @csrf
+                                                                    @method('DELETE')
+                                                                    <button type="submit" class="bg-red-600 text-white p-2 rounded">
+                                                                        Supprimer
+                                                                    </button>
+                                                                </form>
+                                                            </div>
                                                         </div>
+                                                        <button class="close-modal-icon absolute top-2 right-2 text-gray-500 hover:text-gray-700">
+                                                            <i class="fa fa-times"></i>
+                                                        </button>
                                                     </div>
-                                                    <button class="close-modal-icon absolute top-2 right-2 text-gray-500 hover:text-gray-700">
-                                                        <i class="fa fa-times"></i>
-                                                    </button>
                                                 </div>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        
+                                    @endforelse
+                                </x-data-table>
+                            </div>
+
                         </div>
                         <script>
                             document.addEventListener('DOMContentLoaded', function () {
